797da655f14c715e62f4e432df347ffb3d50fc26
PoivronJaune <poivronjaune@gmail.com>
2023-02-27 08:04:45 -0500

    Split csv and parquet

--- hobbytrader/download.py
+++ hobbytrader/download.py
@@ -10,12 +10,13 @@ def sp500():
     tickers_list = tickers().Yahoo.to_list()
     prices_df = minute_prices(tickers_list)
     date_obj = datetime.datetime.now()
-    prices_df.to_parquet(f'DATASET/SP500-{date_obj.date()}.parquet', engine='pyarrow', index=True)
-    prices_df.to_csv(f'DATASET/SP500-{date_obj.date()}.csv', index=True)
+    prices_df.to_parquet(f'DATASET/PARQUET/SP500-{date_obj.date()}.parquet', engine='pyarrow', index=True)
+    prices_df.to_csv(f'DATASET/CSV/SP500-{date_obj.date()}.csv', index=True)
 
 def tsx():
     tickers_list = tsx_tickers().YahooTicker.to_list()
     prices_df = minute_prices(tickers_list)
     date_obj = datetime.datetime.now()
-    prices_df.to_parquet(f'DATASET/TSX-{date_obj.date()}.parquet', engine='pyarrow', index=True)
-    prices_df.to_csv(f'DATASET/TSX-{date_obj.date()}.csv', index=True)
+    prices_df.to_parquet(f'DATASET/PARQUET/TSX-{date_obj.date()}.parquet', engine='pyarrow', index=True)
+    prices_df.to_csv(f'DATASET/CSV/TSX-{date_obj.date()}.csv', index=True)
+    
