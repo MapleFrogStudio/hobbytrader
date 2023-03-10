f064b04dfe485b66ee127040e04db6a943650173
PoivronJaune <poivronjaune@gmail.com>
2023-03-10 13:14:28 -0500

    Scrap ADVFN

--- hobbytrader/symbols.py
+++ hobbytrader/symbols.py
@@ -3,6 +3,11 @@ import requests
 import numpy as np
 import io
 import requests
+import re
+import string
+import time
+
+from bs4 import BeautifulSoup
 
 
 def grab_from_HTML_file():
@@ -49,3 +54,42 @@ def grab_nasdaq_sector_from_github_mfs_dataset(sector='Energy') -> pd.DataFrame:
     sector_df = sector_df.loc[sector_df.Sector == sector].copy()
     return sector_df
 
+def scrap_advfn():
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }    
+    url_base = 'https://ca.advfn.com/investing/stocks/canada/tsx?letter='
+    letters = list(string.ascii_uppercase)
+    letters.append('0')
+    pattern = r'TSX\/(.*?)\/stock-price' # Extract symbol from url
+
+    symbols_list = []
+    for letter in letters:
+        full_url = f'{url_base}{letter}'
+        print(full_url)
+        html_text = requests.get(full_url, headers=header).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+        ul_tags = soup.find_all('ul', class_='investing-list')
+
+        a_tags = []
+        for ul in ul_tags:
+            a_tags += ul.find_all('a')
+        
+        for a_tag in a_tags:
+            name = a_tag.text
+            url = a_tag['href']
+            ticker = re.findall(pattern, url)[0]
+            symbol_info = {"Symbol":ticker, "Name":name, "Url":url}
+            symbols_list.append(symbol_info)
+            #print(f'{ticker: <10} : {name} -> {url}')
+        time.sleep(1)
+
+    symbols_df = pd.DataFrame(symbols_list)
+    return symbols_df
+
+    
+
+if __name__ == '__main__':
+    symbols_df = scrap_advfn()
+    symbols_df.to_csv('test.csv')
