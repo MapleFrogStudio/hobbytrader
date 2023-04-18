323fef399214b401d0347ff64976e2ed4b9cb37c
PoivronJaune <poivronjaune@gmail.com>
2023-04-18 15:05:06 -0400

    FMP API trials

--- hobbytrader/FMPAD.py
+++ hobbytrader/FMPAD.py
@@ -9,25 +9,51 @@ from urllib.request import urlopen
 from dotenv import load_dotenv
 
 load_dotenv()
+api_key = os.getenv('FMPAD_API_KEY')
+print(api_key)
+
 
 def get_jsonparsed_data(url):
     response = urlopen(url)
     data = response.read().decode("utf-8")
-    return json.loads(data)
+    data_df = pd.DataFrame(json.loads(data))
+    return data_df
+
+def get_tsx_tickers():
+    exchange = 'TSX'
+    sector = 'Technology'
+    #url = f'https://financialmodelingprep.com/api/v3/stock-screener?sector={sector}&exchange={exchange}&apikey={api_key}'
+    url = f'https://financialmodelingprep.com/api/v3/stock-screener?exchange={exchange}&apikey={api_key}'
+    response = urlopen(url)
+    data = response.read().decode("utf-8")
+    data_df = pd.DataFrame(json.loads(data))
+    return data_df
 
 
+print('Program working...')
 
-
-api_key = os.getenv('FMPAD_API_KEY')
-print(api_key)
-
-symbol = 'nyse'
-#url = f'https://financialmodelingprep.com/api/v3/income-statement/{symbol}?apikey={api_key}'
-#url = f'https://financialmodelingprep.com/api/v3/quotes/{symbol}?apikey={api_key}'
+#symbol = 'AAPL'
 #url = f'https://financialmodelingprep.com/api/v3/historical-chart/1min/{symbol}?apikey={api_key}'
-url =  f'https://financialmodelingprep.com/api/v3/historical-chart/1min/AAPL?apikey=adccbf975492ed7c363f12ed8d7f3ded'
-data = get_jsonparsed_data(url)
-print(type(data))
-print(data[0:3])
-data_df = pd.DataFrame(data)
-print(data_df.head(3))
+
+#data = get_jsonparsed_data(url)
+#print(f'{type(data)} --- {len(data)} rows)')
+#print(data)
+
+df1 = get_tsx_tickers()
+#df1.to_csv('fmp-tsx.csv')
+#filtered = df1.exchangeShortName == 'TSX'
+#filtered = df1.isEtf == True
+#filtered = df1.isActivelyTrading
+#df1 = df1.loc[filtered].copy()
+#df1 = df1[df1.symbol.str.contains(".TO")]
+print(df1)
+
+#df2 = pd.read_csv('https://raw.githubusercontent.com/MapleFrogStudio/DATASETS/main/STOCK_SYMBOLS/YAHOO/tsx.csv')
+#filtered = df2.Sector == 'Technology'
+#df2 = df2.loc[filtered].copy()
+#print(df2)
+
+#df3 = pd.read_csv('https://raw.githubusercontent.com/MapleFrogStudio/DATASETS/main/STOCK_SYMBOLS/YAHOO/tsxv.csv')
+#filtered = df3.Sector == 'Technology'
+#df3 = df3.loc[filtered].copy()
+#print(df3)
